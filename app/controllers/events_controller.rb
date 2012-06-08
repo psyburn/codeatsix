ac191420ff5b4ebe63db4a520a6342b12ab188a9
Marin Usalj <mneorr@gmail.com>
2012-06-08 18:41:33 +0200

    missed the comma

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
 
     respond_to do |wants|
       wants.html { render 'index' }
-      wants.json { render json: { meetup: Event.meetups.active
+      wants.json { render json: { meetup: Event.meetups.active,
                                   hackathon: Event.hackathons.active }}
     end
   end
